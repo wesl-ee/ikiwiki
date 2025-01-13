wesl-ee/ikiwiki

commit b6edc5ffc523c1775bdde15339f8c760dda4f0ca

Switch from pubdate=pubdate to class=dt-published
At some point in the past, the "pubdate" attribute was defined for
HTML(5). It is no longer, so generating this attribute results in
invalid HTML.

The intent was to signal that time described corresponded to the
document's publication date. The "h-entry" microformat¹ describes
another way to achieve this: apply the class "dt-published" to the
element. The microformat suggests that this *should* be applied to
a <time> tag but does not forbid other elements. When not in HTML5
mode, we generate a <span> tag. Apply the class in that case too.

¹ http://microformats.org/wiki/h-entry

IkiWiki.pm
@@ -1285,11 +1285,13 @@ sub displaytime ($;$$) {
 	my $time=formattime($_[0], $_[1]);
 	if ($config{html5}) {
 		return '<time datetime="'.date_3339($_[0]).'"'.
-			($_[2] ? ' pubdate="pubdate"' : '').
+			($_[2] ? ' class="dt-published"' : '').
 			'>'.$time.'</time>';
 	}
 	else {
-		return '<span class="date">'.$time.'</span>';
+		return '<span class="date'.
+			($_[2] ? ' dt-published' : '').
+			'"">'.$time.'</span>';
 	}
 }
 

commit 2164149383f77c7bbbdaeedcef8e61b6ad7bd38c

Remove extra quote character from date spans
This results in invalid HTML due to the duplicate quote character.

Fixes: commit b6edc5ffc523c1775bdde15339f8c760dda4f0ca

IkiWiki.pm
@@ -1291,7 +1291,7 @@ sub displaytime ($;$$) {
 	else {
 		return '<span class="date'.
 			($_[2] ? ' dt-published' : '').
-			'"">'.$time.'</span>';
+			'">'.$time.'</span>';
 	}
 }
 